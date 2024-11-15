326c692282f9349763778fc89ae12908caf52dd2
Emmanuel Guefif <emmanuel.guefif@demarque.com>
2024-11-14 21:29:29 -0500

    Json parser: binary check done

---
+++ json_parser/src/main.rs
@@ -0,0 +1,25 @@
+pub mod binary_handler;
+use crate::binary_handler::get_check_average;
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 && args.len() > 3 {
+        println!("Usage: cargo run -- file.json [binary.f64]");
+        return;
+    }
+    let filename = &args[1];
+
+    let content = fs::read_to_string(filename).expect("Error while reading json file");
+
+    let binary;
+    if args.len() == 3 {
+        binary = &args[2];
+        let check_average: f64 = get_check_average(binary);
+        println!("Validation");
+        println!("Reference sum: {}", check_average);
+        println!("Diff: {}", check_average);
+    }
+}
