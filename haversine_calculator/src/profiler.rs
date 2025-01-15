3669af622c7983d673cb1dcaa0952df8e09ad079
Emmanuel Guefif <eguefif@fastmail.com>
2025-01-14 21:47:05 -0500

    Improve display

--- haversine_calculator/src/profiler.rs
+++ haversine_calculator/src/profiler.rs
@@ -66,13 +66,15 @@ fn display_total<'a>(iter: &mut impl Iterator<Item = &'a Zone>, last: u64, freq:
 }
 
 fn display_zones<'a>(iter: &mut impl Iterator<Item = &'a Zone>, total: u64, freq: u128) {
+    println!("{:^15}: {:^8} ({}, {})", "label", "time", "actual", "total");
+    println!("-------------------------------------------");
     loop {
         let zone = iter.next();
         if let Some(zone) = zone {
             let child_time = get_child_time(&zone.label);
             let actual_total = zone.elapsed - child_time;
             println!(
-                "{}: {} ms ({:.2}%, {:.2}%)",
+                "{:>15}:{:>7}ms ({:3.2}%, {:3.2}%)",
                 zone.label,
                 (zone.elapsed as u128) / freq,
                 100.0 * (actual_total as f64) / (total as f64),
